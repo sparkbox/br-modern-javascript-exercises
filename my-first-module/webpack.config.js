5c14244a43cce7211fade132802c969e470ba578
Ryan Cromwell <ryan@heysparkbox.com>
2017-05-23 22:37:03 -0400

    fix (my-first-module): compile with es2015 babel preset

--- my-first-module/webpack.config.js
+++ my-first-module/webpack.config.js
@@ -7,7 +7,13 @@ module.exports = {
   }, 
   module: {
     rules: [
-      {test: /\.(js)$/, use: 'babel-loader'}
+      {
+        test: /\.(js)$/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] },
+        }]
+      }
     ]
   }
 };
