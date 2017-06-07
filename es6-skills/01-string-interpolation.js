ea6638a66d8a7b317d9b950ad3b6cf21f968762e
Ryan Cromwell <ryan@heysparkbox.com>
2017-06-07 12:09:36 -0400

    String interpolation example
    closes #5

---
+++ es6-skills/01-string-interpolation.js
@@ -0,0 +1,26 @@
+/* Use string interpolation to replace hard coded values with variables
+ * */
+
+var postUrl = "https://seesparkbox.com/foundry/why_we_write_the_value_of_writing_and_the_editorial_process_that_keeps_us_a";
+var postTitle = "Why We Write: The Value of Writing and the Editorial Process That Keeps Us Accountable";
+var postImage = "https://seesparkbox.com/foundry/uploads/article_uploads/austin_04-27-17.jpg";
+
+var html = ``;
+
+console.log(html);
+
+
+
+
+/* Original Html Source:
+<article class="foundry--article">
+  <a href="https://seesparkbox.com/foundry/why_we_write_the_value_of_writing_and_the_editorial_process_that_keeps_us_a" title="Why We Write: The Value of Writing and the Editorial Process That Keeps Us Accountable" class="foundry--thumb-link">
+    <img class="foundry--thumb" src="https://seesparkbox.com/foundry/uploads/article_uploads/austin_04-27-17.jpg">
+  </a>
+  <header class="foundry--header">
+    <h1 class="foundry--title">
+      <a href="https://seesparkbox.com/foundry/why_we_write_the_value_of_writing_and_the_editorial_process_that_keeps_us_a" class="foundry--link">Why We Write: The Value of Writing and the Editorial Process That Keeps Us Accountable</a>
+    </h1>
+  </header>
+</article>
+*/
